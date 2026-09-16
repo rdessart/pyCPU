14e1718dc75705c123f8371fa1a6817212e988c2
rdessart <romaindessart@gmail.com>
2026-09-16 10:26:25 +0200

    simplification

--- PyCPU/program/compiler.py
+++ PyCPU/program/compiler.py
@@ -2,9 +2,8 @@
 class Compiler:
     def __init__(self):
         self.labels: dict[str, int] = {}
-        self.compiled_program = []
 
-    def load_program(self, source_code: list[tuple]):
+    def load_program(self, source_code: list[tuple]) -> list[tuple]:
         """ Load a parse a program to return full assembly code"""
         pc = 0
         self.labels.clear()
@@ -23,7 +22,7 @@ class Compiler:
             self.compiled_program.append(instruction)
             address += 1
         #second pass:
-        resolved_program = []
+        executable = []
         for instruction in self.compiled_program:
             op = instruction[0].upper()
             args = list(instruction[1:])
@@ -35,5 +34,5 @@ class Compiler:
                     if target not in self.labels:
                         raise ValueError(f"Unknown label: {target}")
                     args[0] = self.labels[target]
-            resolved_program.append((op, *args))
-        self.compiled_program = resolved_program
+            executable.append((op, *args))
+        return self.executable
