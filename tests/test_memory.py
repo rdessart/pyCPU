b4f3acc0912143b151cebf4b62972b8e0369e192
rdessart <romaindessart@gmail.com>
2026-09-16 19:53:47 +0200

    added memory tests

---
+++ tests/test_memory.py
@@ -0,0 +1,42 @@
+from PyCPU.components.memory import Memory
+import pytest
+
+def test_write_to_memory():
+    mem = Memory(bits=8, size=2)
+    mem.write(address=0, value=0xFF)
+    assert mem.data[0] == 0xFF
+    assert mem.data[1] == -1
+
+def test_write_to_invalid_memory_too_large():
+    mem = Memory(bits=8, size=2)
+    with pytest.raises(ValueError, match="Address should be less than 2"):
+        mem.write(address=3, value=0xFF)
+    assert mem.data[0] == -1
+    assert mem.data[1] == -1
+
+def test_write_to_invalid_memory_too_low():
+    mem = Memory(bits=8, size=2)
+    with pytest.raises(ValueError, match="Address should be > 0"):
+        mem.write(address=-1, value=0xFF)
+    assert mem.data[0] == -1
+    assert mem.data[1] == -1
+
+def test_read_to_memory():
+    mem = Memory(bits=8, size=2)
+    mem.data[0] = 0xFF
+    assert mem.read(0) == 0xFF
+
+def test_read_to_invalid_memory_too_large():
+    mem = Memory(bits=8, size=2)
+    with pytest.raises(ValueError, match="Address should be less than 2"):
+        mem.read(address=3)
+
+def test_read_to_invalid_memory_too_low():
+    mem = Memory(bits=8, size=2)
+    with pytest.raises(ValueError, match="Address should be > 0"):
+        mem.read(address=-1)
+
+def test_read_to_uninitialized():
+    mem = Memory(bits=8, size=2)
+    with pytest.raises(ValueError, match="Trying to access un-initalized memory at 0"):
+        mem.read(address=0)
