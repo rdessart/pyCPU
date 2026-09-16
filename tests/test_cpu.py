2186a0e9f295fc7475ecef5a837a7b83c6269d96
rdessart <romaindessart@gmail.com>
2026-09-16 11:24:14 +0200

    added CPU test

---
+++ tests/test_cpu.py
@@ -0,0 +1,69 @@
+from PyCPU.components.cpu import CPU
+import pytest
+
+def test_cpu_register_init():
+    cpu = CPU(2)
+    source = [
+            ("MOV", "R0", 10),
+            ("MOV", "R1", 1),
+        ]
+    cpu.load_program(source)
+    cpu.execute()
+
+    assert cpu.registers["R0"].get() == 10
+    assert cpu.registers["R1"].get() == 1
+
+def test_cpu_register_add():
+    cpu = CPU(2)
+    source = [
+            ("MOV", "R0", 10),
+            ("MOV", "R1", 1),
+            ("ADD", "R1", "R0")
+        ]
+    cpu.load_program(source)
+    cpu.execute()
+
+    assert cpu.registers["R0"].get() == 10
+    assert cpu.registers["R1"].get() == 11
+
+def test_cpu_register_cmp_less():
+    cpu = CPU(2)
+    source = [
+            ("MOV", "R0", 10),
+            ("MOV", "R1", 1),
+            ("CMP", "R1", "R0")
+        ]
+    cpu.load_program(source)
+    cpu.execute()
+
+    assert cpu.zero_flag == False
+    assert cpu.less_flag == True
+    assert cpu.greater_flag == False
+
+def test_cpu_register_cmp_bigger():
+    cpu = CPU(2)
+    source = [
+            ("MOV", "R0", 10),
+            ("MOV", "R1", 1),
+            ("CMP", "R0", "R1")
+        ]
+    cpu.load_program(source)
+    cpu.execute()
+
+    assert cpu.zero_flag == False
+    assert cpu.less_flag == False
+    assert cpu.greater_flag == True
+
+def test_cpu_register_cmp_equal():
+    cpu = CPU(2)
+    source = [
+            ("MOV", "R0", 10),
+            ("MOV", "R1", 10),
+            ("CMP", "R0", "R1")
+        ]
+    cpu.load_program(source)
+    cpu.execute()
+
+    assert cpu.zero_flag == True
+    assert cpu.less_flag == False
+    assert cpu.greater_flag == False
